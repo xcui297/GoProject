fa806ceb171c2ccf2c9b934e1bc5dcc58d897167
echoziyicui <ziyic@andrew.cmu.edu>
2017-11-30 23:26:55 -0500

    add func2 and give 'finalPrice,supplement' for func1 by Echo

--- coreFunc.go
+++ coreFunc.go
@@ -8,6 +8,8 @@ import (
   "io"
   "strings"
   "strconv"
+	"log"
+	"time"
 
 	"github.com/draffensperger/golp"
 )
@@ -33,6 +35,7 @@ type Paint struct {
 	G int
 	B int
 	amount float64
+	price int
 }
 
 type Component struct {
@@ -40,8 +43,8 @@ type Component struct {
 	percentage float64
 	amount float64
 	inStock bool
+	supplement float64
 }
-
 /*==============================================================================
  * 1. Load repository
  *
@@ -57,7 +60,6 @@ func ReadFile(filename string) (bool,[]Pigment) {
 	if err != nil {
 		return noError, pigments
 	}
-
   // Read the csv file line by line
 	lines := csv.NewReader(bufio.NewReader(in))
 	for {
@@ -142,29 +144,31 @@ func SpaceFieldsJoin(str string) string {
     return strings.Join(strings.Fields(str), "")
 }
 
+
 /*==============================================================================
  * 2. Mix to get the target color
  *
  *============================================================================*/
-// FindComponents uses linear programming to find pigments that can mix together in certain proportion
-// to give the targret Color.
+// FindComponents uses linear programming to find pigments that can mix together
+// in certain proportion to give the targret Color.
 // It returns two boolean value indicating whether can find components from
 // the given repository and whether all components are in stock, respectively.
-// It also returns a slice of those found components.
-func FindComponents(R, G, B int, targetAmount float64, repository []Pigment) (bool, bool, []Component) {
+// It returns a slice of those found components.
+// It also returns the final price
+func FindComponents(R, G, B int, targetAmount float64, repository []Pigment) (bool, bool, []Component, int) {
 	var targetColor Paint
 	targetColor.R = R
 	targetColor.G = G
 	targetColor.B = B
 	targetColor.amount = targetAmount
-	vars := SolveLPFunc1(targetColor, repository)
-	return InterpretFunc1LPResults(vars, targetAmount, repository)
+	vars, finalUnitPrice := SolveLPFunc1(targetColor, repository)
+	return InterpretFunc1LPResults(vars, finalUnitPrice, targetAmount, repository)
 }
 
-// SolveLP gives the component proportions at a minimized price when it is
+// SolveLPFunc1 gives the component proportions at a minimized price when it is
 // constrained by the targeted R,G,B values and the same total unit amount.
-func SolveLPFunc1(targetColor Paint, repository []Pigment) []float64 {
-	// Make a new linear program structure.
+func SolveLPFunc1(targetColor Paint, repository []Pigment) ([]float64, int) {
+	// Make a new liear program structure.
 	numPigments := len(repository)
 	lp := golp.NewLP(0, numPigments)
 
@@ -207,20 +211,20 @@ func SolveLPFunc1(targetColor Paint, repository []Pigment) []float64 {
 	// combination. We convert the minimization to a maximination by setting the
 	// coefficents of price to negative.
 	lp.SetObjFn(rowPrice)
-	vars := SolveLP(*lp)
+	vars, finalUnitPrice := SolveLP(*lp)
 
-	return vars
+	return vars, finalUnitPrice
 }
 
-func SolveLP(lp golp.LP) []float64 {
+func SolveLP(lp golp.LP) ([]float64, int) {
 	// Maximizethe objective function, restricted by constraints of lp
 	lp.SetMaximize()
 
 	// Solve the linear programming.
 	lp.Solve()
 	vars := lp.Variables()
-
-	return vars
+	finalUnitPrice := -int(lp.Objective())
+	return vars, finalUnitPrice
 }
 
 // InterpretLPResults evaluates the results from linear programming and gives:
@@ -230,7 +234,7 @@ func SolveLP(lp golp.LP) []float64 {
 // required amount of the target color.
 // 3. a slice of Comonent, each of which gives the pigment, the proportion, the
 // required amount, the stock status.
-func InterpretFunc1LPResults(vars []float64, targetAmount float64, repository []Pigment) (bool, bool, []Component) {
+func InterpretFunc1LPResults(vars []float64, finalUnitPrice int, targetAmount float64, repository []Pigment) (bool, bool, []Component, int) {
 	var exists, inStock bool
 	composition:= make([]Component, 0)
 
@@ -241,7 +245,8 @@ func InterpretFunc1LPResults(vars []float64, targetAmount float64, repository []
 		// It also returns an empty slice of Component.
 		if vars[idx] < 0.0 {
 			composition = []Component{}
-			return exists, inStock, composition
+			finalUnitPrice = 0
+			return exists, inStock, composition, finalUnitPrice
 
 		// Add each component with positve percentage into composition.
 		// Calculate their required amount according to the percentage and amount of
@@ -255,6 +260,7 @@ func InterpretFunc1LPResults(vars []float64, targetAmount float64, repository []
 			// Compare the amount required with the stock. If in stock, return true.
 			if stock < component.amount {
 				component.inStock = false
+				component.supplement = component.amount - stock
 			} else {
 				component.inStock = true
 			}
@@ -268,24 +274,138 @@ func InterpretFunc1LPResults(vars []float64, targetAmount float64, repository []
 	// Check whether the composition is empty.
 	if len(composition) > 0 {
 		exists = true
+	} else {
+		composition = []Component{}
+		finalUnitPrice = 0
+		return exists, inStock, composition, finalUnitPrice
 	}
 
 	// Check whether all the components are in stock.
 	for _,component := range composition {
 		if component.inStock == false {
 			inStock = false
-			return exists, inStock, composition
+			return exists, inStock, composition, finalUnitPrice
 		}
 	}
 
 	inStock = true
-	return exists,inStock, composition
+	return exists,inStock, composition,finalUnitPrice
 }
-
 /*==============================================================================
- *
+ * 3. Off-color hit
  *
  *============================================================================*/
+ func OffColorHit(R0, G0, B0, R, G, B, currentPrice int, repository []Pigment) (bool,float64, []Component, int) {
+ 	var currentColor,targetColor Paint
+ 	currentColor.R = R0
+ 	currentColor.G = G0
+ 	currentColor.B = B0
+ 	currentColor.price = currentPrice
+
+ 	targetColor.R = R
+ 	targetColor.G = G
+ 	targetColor.B = B
+ 	vars, finalUnitPrice := SolveLPFunc2(currentColor, targetColor, repository)
+ 	return InterpretFunc2LPResults(vars, finalUnitPrice, repository)
+ }
+
+
+ func SolveLPFunc2(currentColor, targetColor Paint, repository []Pigment) ([]float64, int) {
+ 	// Make a new liear program structure.
+ 	numPigments := len(repository)
+ 	lp := golp.NewLP(0, numPigments+1)
+
+ 	// Make slices to hold the coefficients and the objective function.
+ 	// N indicates the amount of each pigment.
+ 	// rowCurrentColor requires currentColor must be used.
+ 	rowR := make([]float64,0)
+ 	rowG := make([]float64,0)
+ 	rowB := make([]float64,0)
+ 	rowN := make([]float64,0)
+ 	rowCurrentColor:= make([]float64,0)
+ 	rowPrice:= make([]float64, 0)
+ 	// Add coefficients for currentColor to the constraints
+ 	rowR = append(rowR, float64(currentColor.R))
+ 	rowG = append(rowG, float64(currentColor.G))
+ 	rowB = append(rowB, float64(currentColor.B))
+ 	rowN = append(rowN, 1.0)
+ 	rowCurrentColor = append(rowCurrentColor, 1.0)
+ 	rowPrice = append(rowPrice,-float64(currentColor.price))
+ 	// Add coefficents for pigment repository to the above slices.
+ 	for _,pigment := range repository {
+ 		rowR = append(rowR, float64(pigment.R))
+ 		rowG = append(rowR, float64(pigment.G))
+ 		rowB = append(rowR, float64(pigment.B))
+ 		rowN = append(rowN, 1.0)
+ 		rowCurrentColor = append(rowCurrentColor, 0.0)
+		rowPrice = append(rowPrice, -float64(pigment.price))
+ 	}
+
+ 	// Add constraints to the linear programming.
+ 	// This constraint requires pigments to give the target value of R.
+ 	lp.AddConstraint(rowR, golp.EQ, float64(targetColor.R))
+ 	// This constraint requires pigments to give the target value of G.
+ 	lp.AddConstraint(rowG, golp.EQ, float64(targetColor.G))
+ 	// This constraint requires pigments to give the target value of B.
+ 	lp.AddConstraint(rowB, golp.EQ, float64(targetColor.B))
+ 	// This constraint requires the total amount for each pigment combination
+ 	// to be the same, which is 1.0.
+ 	lp.AddConstraint(rowN, golp.EQ, 1.0)
+ 	// currentColor must be used.
+ 	lp.AddConstraint(rowCurrentColor,golp.GE, 0.001)
+ 	lp.SetObjFn(rowPrice)
+
+ 	return SolveLP(*lp)
+ }
+
+ // InterpretLPFunc2 Results evaluates the results from linear programming and gives:
+ // 1. a boolean value, which indicates the feasability to find components for the
+ // target value.
+ // 2. a percentage of the currentcolor.
+ // 3. a slice of Comonent, each of which gives the pigment, the proportion, the
+ // required amount, the stock status.
+ func InterpretFunc2LPResults(vars []float64, finalUnitPrice int, repository []Pigment) (bool,float64, []Component, int) {
+ 	var exists bool
+	var currentColorProportion float64
+ 	composition:= make([]Component, 0)
+
+ 	for idx := range vars {
+ 		// Check negative values. Since we do not want negative values, simply return
+ 		// exists = false
+ 		// It also returns an empty slice of Component.
+ 		if vars[idx] < 0.0 {
+ 			composition = []Component{}
+			finalUnitPrice = 0
+ 			return exists, 0.0, composition, finalUnitPrice
+ 		// Add each component with positve percentage into composition.
+ 		} else if vars[idx] > 0.0 {
+ 			if idx == 0 {
+ 			currentColorProportion = vars[idx]
+ 			} else {
+ 				var component Component
+ 				component.pigment = &(repository[idx])
+ 				component.percentage = vars[idx]
+ 				composition = append(composition, component)
+ 			}
+ 		// "0" indicates this pigment is not used.
+ 		} else {
+ 			continue
+ 		}
+ 	}
+ 	// Check whether the composition is empty.
+ 	if len(composition) > 1 {
+ 		exists = true
+ 	} else {
+		composition = []Component{}
+		finalUnitPrice = 0
+		return exists, 0.0, composition, finalUnitPrice
+	}
+ 	return exists, currentColorProportion, composition, finalUnitPrice
+ }
+/*==============================================================================
+* 4.
+*
+*=============================================================================*/
 //MixColor takes a slice of pigments and their weights
 //and returns the mixed new color, as well as the price per gal
 func MixColor(p []Pigment, weight []float64) (Paint, float64) {
@@ -327,11 +447,15 @@ func main() {
 		fmt.Println("Load repository: Failed.")
 	}
 
+	//------------------test for func 1-------------------------------------------
 	R := 128
 	G := 128
 	B := 128
 	targetAmount := 20.0
-	exists, inStock, composition := FindComponents(R, G, B, targetAmount, repository)
+	start1 := time.Now()
+	exists, inStock, composition, finalUnitPrice := FindComponents(R, G, B, targetAmount, repository)
+	elapsed1 := time.Since(start1)
+	log.Printf("func1 took %v", elapsed1)
 	if exists == true {
 		fmt.Fprintf(os.Stdout, "Components found for targetColor from the given repository.\n")
 	} else {
@@ -349,8 +473,30 @@ func main() {
 		status := "in stock"
 		if component.inStock == false {
 			status = "out of stock"
-		}
+			fmt.Fprintf(os.Stdout,"Name = %v, ID = %v, proportion = %v, required amount = %v, %v, supplement = %v \n",
+			pigment.name, pigment.ID, component.percentage, component.amount, status, component.supplement)
+		} else {
 		fmt.Fprintf(os.Stdout,"Name = %v, ID = %v, proportion = %v, required amount = %v, %v \n",
 		pigment.name, pigment.ID, component.percentage, component.amount, status)
+		}
 	}
+	fmt.Fprintf(os.Stdout, "The lowest unit price for this target color is %v.", finalUnitPrice)
+	fmt.Println()
+	//------------------test for func 2-------------------------------------------
+	R0 := 128
+	G0 := 120
+	B0 := 110
+	currentP := 20
+	R = 128
+	G = 110
+	B = 120
+	exists2, currentProp, composition2, finalUnitPrice := OffColorHit(R0, G0, B0, R, G, B, currentP, repository)
+	fmt.Println(exists2, currentProp)
+	for _, component := range composition2 {
+		pigment := *(component.pigment)
+		fmt.Fprintf(os.Stdout,"Name = %v, ID = %v, proportion = %v\n",
+		pigment.name, pigment.ID, component.percentage)
+	}
+	fmt.Println("final unit price is %v", finalUnitPrice)
+
 }
